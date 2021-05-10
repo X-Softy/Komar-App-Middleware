4d35dbca55c9fd2f0d776f0cc8f5c3727b69e328
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2021-05-10 21:03:33 +0400

    add temps table to test Firestore

--- komar-app/src/main.ts
+++ komar-app/src/main.ts
@@ -3,6 +3,9 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const regionalFunctions = functions.region('europe-west3');
 
 const server = express();
 
@@ -18,4 +21,27 @@ createNestServer(server)
   .then((v) => console.log('Nest Ready'))
   .catch((err) => console.error('Nest broken', err));
 
-export const api = functions.https.onRequest(server);
+export const api = regionalFunctions.https.onRequest(server);
+
+// Temps Table Services
+const temps = express();
+admin.initializeApp();
+
+temps.get('/', async (request, response) => {
+  const snapshot = await admin.firestore().collection('temps').get();
+  const temps = [];
+  snapshot.forEach((document) => {
+    const id = document.id;
+    const data = document.data();
+    temps.push({ id, ...data });
+  });
+  response.status(200).send(JSON.stringify(temps));
+});
+
+temps.post('/', async (request, response) => {
+  const temp = request.body;
+  await admin.firestore().collection('temps').add(temp);
+  response.status(201).send();
+});
+
+export const temp = regionalFunctions.https.onRequest(temps);
